f5d89488b3a5547c0bfbf7b56e77017a95a61d84
Sergey Mochalov <proweb@users.noreply.github.com>
2024-11-14 18:53:24 +0300

    init commit
    Project base

---
+++ cron/fileDo.php
@@ -0,0 +1,20 @@
+<?php
+// Filename MUST have Tasks.php as suffix
+use Crunz\Schedule;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+# Init
+$schedule = new Schedule();
+$storage = dirname(__DIR__) . '/storage';
+$timestamp = time();
+
+
+$task = $schedule->run(function () use ($storage, $timestamp) {
+    file_put_contents("{$storage}/{$timestamp}.txt", $timestamp);
+});
+$task
+    ->everyFiveMinutes()
+    ->description('Создание файла каждые 5 минут.');
+
+return $schedule;
